0f7982e2d9f09e6a0b6a9507d808476f5d42cd8f
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-03 15:32:50 +0000

    Day 45 Interface

--- src/day45_Abstraction_Interface/shape/Cube.java
+++ src/day45_Abstraction_Interface/shape/Cube.java
@@ -1,4 +1,33 @@
 package day45_Abstraction_Interface.shape;
 
-public class Cube {
+public class Cube extends Shape implements Volume {
+
+    private double side;
+
+    public Cube(String name, double side) {
+        super(name);
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return 6*side*side;
+    }
+    @Override
+    public double perimeter() {
+        return 12*side;
+
+    }
+
+    public String toString() {
+        return "Cube{" + super.toString()+
+                "side=" + side +
+                '}';
+    }
+
+
+    @Override
+    public double volume() {
+        return 0;
+    }
 }
