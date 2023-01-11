1f7d4793165c87698d3a1fd819318cbf0dac4890
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-11 22:23:37 +0000

    Day 46 Polymorphism

---
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -0,0 +1,58 @@
+package day46_Polymorphism;
+
+
+public class PolymorphismIntro {
+    public static void main(String[] args) {
+        String str = "Wooden Spoon";
+
+        Integer n1 = 2;
+
+        Double n2 = 5.5;
+
+        Boolean r1 = true;
+
+
+        // employee.bugReport();
+
+        /*
+            WebDriver driver1 = new ChromeDriver();
+            WebDriver driver2 = new ChromeDriver();
+            driver1.get(amazon)
+            driver2.get(google)
+            Animal animal = new Dog();
+            animal.drink()
+            Animal animal = new Cat()
+            animal.drink()
+            Animal animal = new Tiger()
+            animal.drink()
+         */
+
+
+
+
+
+
+        /*
+        WebDriver driver:
+            if(driver instanceof Chrome){
+                Chrome browser will open
+            }else if(driver instanceof firefox){
+                Firefox browser will open
+            }
+            ...
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
